fe8c5d89304546472a43eef5e2ad55534f88be3c
AllenLyu <a314007@gmail.com>
2020-02-26 18:19:13 +0800

    HW_week7

--- hw_week7.py
+++ hw_week7.py
@@ -1,9 +1,18 @@
 import random
 
 def hw_week7(secret_number):
-    # Your code here
-    pass
-
+  y=int(0)
+  while True:
+    y=y+1
+    print('Take a guess.')
+    x=int(input())
+    if secret_number > x:
+      print('Your guess is too low.')
+    elif secret_number < x:
+      print('Your guess is too high.')
+    else:
+      print('Good job! You guessed my number '+ str(y) +' in guesses!')
+      break
 
 if __name__ == '__main__':
-    hw_week7(random.randint(1, 100))
+  hw_week7(random.randint(1, 100))
